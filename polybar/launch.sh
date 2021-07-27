c8da2dd050943e77597c975cc374b4b078281a1b
Pinkieqt <izyman2@gmail.com>
2021-07-27 22:28:42 +0000

    Arch update - move from Archcraft

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,6 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Launch the bar
-STYLE="smirage"
+# Terminate already running bar instances
+killall -q polybar
 
-bash "$HOME"/.config/polybar/"$STYLE"/launch.sh
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar bar &
---
+++ polybar/old/launch.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Launch the bar
+STYLE="smirage"
+
+bash "$HOME"/.config/polybar/"$STYLE"/launch.sh
